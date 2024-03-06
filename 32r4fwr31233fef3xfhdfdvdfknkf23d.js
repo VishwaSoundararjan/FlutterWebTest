67844c6cb1e62eb1910026433d3af26019bc909f
VishwaSoundararjan <134969718+VishwaSoundararjan@users.noreply.github.com>
2024-03-06 18:06:33 +0530

    Update 32r4fwr31233fef3xfhdfdvdfknkf23d.js

--- 32r4fwr31233fef3xfhdfdvdfknkf23d.js
+++ 32r4fwr31233fef3xfhdfdvdfknkf23d.js
@@ -3,7 +3,7 @@
 console.log("FIle Loaded");
 
 window.ReWebSDK = {}
-window.ReWebSDK.getVersion() = function() {
+window.ReWebSDK.getVersion = function() {
     console.log("getVersion called: Flutter web sdk version - 1.0.0");
     return "Flutter web sdk version - 1.0.0";
 }
