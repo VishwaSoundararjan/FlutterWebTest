b286afafe1e135df05a3266182d859b5093b3885
VishwaSoundararjan <134969718+VishwaSoundararjan@users.noreply.github.com>
2024-03-21 17:48:38 +0530

    created new js files

---
+++ s460fsr31233fef3xfhdfdqdfknkfcd45.js
@@ -0,0 +1,9 @@
+
+
+console.log("FIle Loaded");
+
+window.ReWebSDK = {}
+window.ReWebSDK.getVersion = function() {
+    console.log("getVersion called: Flutter web sdk version - 6.0.0");
+    return "Flutter web sdk version - 6.0.0";
+}
